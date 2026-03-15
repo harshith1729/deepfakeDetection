88834a63e1e1345dd2ad543586d86155d8683937
Harshith N <harshith1440@gmail.com>
2026-03-15 22:55:06 +0530

    Twillo functionality wroking fine

--- alert_system/twilio_alert.py
+++ alert_system/twilio_alert.py
@@ -1,5 +1,17 @@
 from twilio.rest import Client
-from .config import *
+import os
+from dotenv import load_dotenv
+
+# load .env variables
+load_dotenv()
+
+ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+TWILIO_PHONE = os.getenv("TWILIO_PHONE")
+OWNER_PHONE = os.getenv("OWNER_PHONE")
+DOOR_NAME = os.getenv("DOOR_NAME")
+HOUSE_NAME = os.getenv("HOUSE_NAME")
+CITY = os.getenv("CITY")
 
 def send_alert():
 
